2bd24d1f4e136c6e493c6619cd523543c8303293
Ade <adelinapopa02@gmail.com>
2026-04-28 18:13:05 +0200

    Added FairFFT and MRFairFFT and completed main

--- src/G70HW1.java
+++ src/G70HW1.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 public class G70HW1 {
 
-    public static void main (String[] args){
+    public static void main (String[] args) {
         if (args.length != 4)
             throw new IllegalArgumentException("USAGE: file_path KA KB L");
 
@@ -30,12 +30,74 @@ public class G70HW1 {
         long NB = inputPoints.filter(p->p._2().equals("B")).count();
         System.out.println("N = " + N + ", NA = " + NA + ", NB = " + NB);
 
+        long start = System.currentTimeMillis();
+        ArrayList<Tuple2<Vector, String>> solution = MRFairFFT(inputPoints, KA, KB, L);
+        long end = System.currentTimeMillis();
+
+        for (Tuple2<Vector, String> center : solution) {
+            double[] coords = center._1().toArray();
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < coords.length; i++) {
+                sb.append(coords[i]).append(i == coords.length - 1 ? "" : ",");
+            }
+            System.out.println("Center = [" + sb + "] Label = " + center._2());
+        }
+
+        double objective = computeObjective(inputPoints, solution);
+        System.out.println("Objective function = " + objective);
+        System.out.println("Running time of MRFairFFT = " + (end - start) + " ms");
     }
 
     public static Tuple2<Vector, String> parseLine(String line) {
-        String[] parts = line.split(",");
-        double[] coords = new double[parts.length - 1];
-        for (int i = 0; i < parts.length - 1; i++) coords[i] = Double.parseDouble(parts[i]);
-        return new Tuple2<>(Vectors.dense(coords), parts[parts.length - 1]);
+        String[] elements = line.split(",");
+        double[] coords = new double[elements.length - 1];
+        for (int i = 0; i < elements.length - 1; i++) coords[i] = Double.parseDouble(elements[i]);
+        return new Tuple2<>(Vectors.dense(coords), elements[elements.length - 1]);
+    }
+
+    public static ArrayList<Tuple2<Vector, String>> FairFFT(List<Tuple2<Vector, String>> P, int KA, int KB) {
+        int n = P.size();
+        ArrayList<Tuple2<Vector, String>> centers = new ArrayList<>();
+        double[] minDistances = new double[n];
+        for (int i = 0; i < n; i++) minDistances[i] = Double.POSITIVE_INFINITY;
+        int countA = 0, countB = 0;
+        for (int i = 0; i < (KA + KB); i++) {
+            int bestIndex = -1;
+            double maxDist = -1.0;
+            for (int j = 0; j < n; j++) {
+                String label = P.get(j)._2();
+                boolean isLegal = (label.equals("A") && countA < KA) || (label.equals("B") && countB < KB);
+                if (isLegal && (centers.isEmpty() || minDistances[j] > maxDist)) {
+                    maxDist = minDistances[j];
+                    bestIndex = j;
+                }
+            }
+            Tuple2<Vector, String> newCenter = P.get(bestIndex);
+            centers.add(newCenter);
+            if (newCenter._2().equals("A")) countA++; else countB ++;
+            for(int j = 0; j < n; j++) {
+                minDistances[j]= Math.min(minDistances[j], Vectors.sqdist(P.get(j)._1(), newCenter._1()));
+            }
+        }
+        return centers;
+    }
+
+    public static ArrayList<Tuple2<Vector, String>> MRFairFFT(JavaPairRDD<Vector, String> P, int KA, int KB, int L) {
+        List<Tuple2<Vector, String>> coreset = P.mapPartitionsToPair(partition -> {
+            List<Tuple2<Vector, String>> points = new ArrayList<>();
+            while (partition.hasNext()) points.add(partition.next());
+            return FairFFT(points, KA, KB).iterator();
+        }).collect();
+        return FairFFT(coreset, KA, KB);
+    }
+
+    public static double computeObjective(JavaPairRDD<Vector, String> points, List<Tuple2<Vector, String>> centers) {
+        return Math.sqrt(points.map(p -> {
+            double minDistSq = Double.POSITIVE_INFINITY;
+            for (Tuple2<Vector, String> c : centers) {
+                minDistSq = Math.min(minDistSq, Vectors.sqdist(p._1(), c._1()));
+            }
+            return minDistSq;
+        }).reduce(Math::max));
     }
 }
